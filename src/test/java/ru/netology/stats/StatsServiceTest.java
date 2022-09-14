f978a4dbe96895a0d95bc0039154ee129f2a4b64
Елена Кливер <volandmart@mail.ru>
2022-09-14 18:41:01 +0500

    NewProg

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,73 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void ShoudСalculateSum() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 180;
+        long actual = service.getSalesAmount(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ShoudСalculateAverageSum() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 15;
+        long actual = service.average(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+
+
+    @Test
+    public void ShoudСalculateMaxSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 8;
+        long actual = service.maxSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ShoudBelowAverage() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        long actual = service.belowAverage(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ShoudAboveAverage() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        long actual = service.aboveAverage(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
